b821fabfa932e5526e03d821152987c42d63f2d1
marco.schmidl <marcoschmidl@pm.me>
2024-09-16 22:26:07 +0200

    feat: logging for docker

--- demo-dockerimage-pip-script/app.py
+++ demo-dockerimage-pip-script/app.py
@@ -1,5 +1,16 @@
 import time
+import logging
+
+# Logger konfigurieren
+logging.basicConfig(
+    level=logging.INFO,  # Log-Level auf INFO setzen
+    format="%(asctime)s [%(levelname)s] %(message)s",  # Format für Logs
+    handlers=[
+        logging.FileHandler("app.log"),  # Log-Datei
+        logging.StreamHandler()  # Ausgabe auf der Konsole
+    ]
+)
 
 while True:
-    print("Hallo, Welt!")
-    time.sleep(2)  # 30 Sekunden warten
+    logging.info("Hallo, Welt!")
+    time.sleep(1)  # Alle 30 Sekunden "Hallo, Welt!" loggen
